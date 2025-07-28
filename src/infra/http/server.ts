db9157417c7bed7a9483b152d03cb68b1468658c
rfazevedo <rogerio@rogerioazevedo.net>
2025-07-28 14:57:10 -0400

    feat: add upload route & swagger

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,18 +1,63 @@
+import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    reply.status(400).send({
+      message: 'Validation error',
+      issues: error.validation,
+    })
+  }
+
+  console.error('Error occurred:', error)
+
+  reply.status(500).send({
+    message: 'An unexpected error occurred',
+  })
+})
+
 server.register(fastifyCors, { origin: '*' })
+server.register(fastifyMultipart)
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server API',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(uploadImageRoute)
 
 server.get('/', () => {
   return 'Hello World'
 })
 
-server.listen({ port: 3333 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server is running on ${address}`)
-})
+console.log('Starting server...', env.DATABASE_URL)
+
+server
+  .listen({ port: 3333, host: '0.0.0.0' })
+  .then(() => console.log('Server is running'))
